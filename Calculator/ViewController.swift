1ab3abb21b1ef6d74edc7701e2ab74502c10063f
Noah Pope <npope@tutanota.com>
2022-09-22 08:06:26 -0400

    made guarded displayValue a global computed property. Made isInt a bool by comparing currentDisplayValue to its floored version

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -22,6 +22,15 @@ class ViewController: UIViewController {
     //you wont accidently modify something via another class
     private var isFinishedTypingNumber: Bool = true
 
+    private var displayValue: Double {
+        //get currentvalue from displayLabel.text
+        get {
+            guard let currentValue = Double(displayLabel.text!) else {
+                fatalError("could not convert display label text to a Double")
+            }
+            return currentValue
+        }
+    }
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
@@ -30,20 +39,16 @@ class ViewController: UIViewController {
         //isFinishedTypingNumber = "tracker"
         isFinishedTypingNumber = true
         
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert display label text to a Double.")
-        }
-        
         if let calcMethod = sender.currentTitle {
             if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
+                displayLabel.text = String(displayValue * -1)
             }
             else if calcMethod == "AC" {
                 displayLabel.text = "0"
             }
             else if calcMethod == "%" {
                 
-                displayLabel.text = String(number/100)
+                displayLabel.text = String(displayValue/100)
             }
         }
                 
@@ -59,15 +64,19 @@ class ViewController: UIViewController {
                 isFinishedTypingNumber = false
             } else {
                 if buttonValue == "." {
-                                        
-                    decimal.isEnabled = false
-                    
+                                                            
                     guard let currentDisplayValue = Double(displayLabel.text!) else {
                         fatalError("could not convert display label text to a Double")
                     }
                     
-                    let isInt = floor(currentDisplayValue)
+                    //so basically isInt = true or false
+                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    
+                    if !isInt {
+                        return
+                    }
                 }
+                
                 displayLabel.text = displayLabel.text! + buttonValue
             }
             
